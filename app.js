87e74ae5544e52d3e4b5c5584aac5c7afa56856f
Michael Carr <mcarr2468@gmail.com>
2021-03-23 20:23:14 -0500

    got next btn working

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-let prevSlideBtn = document.querySelector(".prev-slide-btn");
-let NextSlideBtn = document.querySelector(".next-slide-btn");
-let slideItems = document.querySelector(".image-slider-items");
-let slideItemArr = Array.from(document.querySelectorAll(".image-slider-item"));
-
-
-let containerInner = document.querySelector(".container__inner")
-let imageContainer = document.querySelector(".images-container")
-let dots = document.querySelector(".dots");
-let dotAllArr = Array.from(document.querySelectorAll(".dot"));
-
-
-
-
 /*TODO
 1. get the slides to move on click of prev / next btn
 
@@ -27,15 +13,53 @@ let dotAllArr = Array.from(document.querySelectorAll(".dot"));
 */
 
 
-function sliderFunctionality(event) {
-    let currentSlide = document.querySelector(".image-slider-item--current");
-    let prevSlide = currentSlide.previousElementSibling;
-    let nextSlide = currentSlide.nextElementSibling;
 
 
+
+
+let slides = document.querySelector(".image-slider-items").children;
+let prevSlideBtn = document.querySelector(".prev-slide-btn");
+let nextSlideBtn = document.querySelector(".next-slide-btn");
+let slidesTotalAmt = slides.length;
+let imageContainer = document.querySelector(".images-container");
+// console.log(slides.length)
+
+
+
+
+let index = 0;
+prevSlideBtn.addEventListener("click", function() {
+    sliderFunctionality("prev")
+});
+
+nextSlideBtn.addEventListener("click", function() {
+    sliderFunctionality("next")
+});
+
+
+let sliderFunctionality = slideDirection => {
+    console.log(index)
+
+    if(slideDirection === "next") {
+        index++;
+        if(index === slidesTotalAmt) {
+           index = 0;
+           console.log(index)
+        } else {
+            // if(index === 0) {
+            //     index = slidesTotalAmt - 1;
+            // } else {
+            //     index--;
+            // }
+        }
+
+        for(let i = 0; i < slides.length; i++) {
+            slides[i].classList.remove("image-slider-item--current")
+        }
+        slides[index].classList.add("image-slider-item--current")
+        imageContainer.style = `transform: translateX(${-Number(index +  "00")}%)`;
+    }
 }
 
 
-
-
-
+console.log(-Number(index +  "00") )
